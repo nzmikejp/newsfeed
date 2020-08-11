822aec66ade9c5d098df857918ad8ec3e9f313b0
Mike Jackson-Poter <nzmikejp@gmail.com>
2020-08-11 16:48:30 +1200

    design in the process

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import logo from './assets/images/logo.svg'
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tab'
 import './assets/css/style.css'
 
 //NewsAPI: https://gnews.io/docs/v3#news-from-topic
@@ -10,7 +12,15 @@ class App extends Component {
     super(props)
 
     this.state = {
-
+      articles: [
+        {
+          id: 1,
+          title: "COVID-19 testing 'very likely' when Royal Caribbean returns to cruising, executive says",
+          description: "Royal Caribbean Group is set to begin COVID-19 testing on its ships when cruise operations return, according to an executive.",
+          publishedAt: "2020-08-10 18:14:00 UTC",
+          image: "https://images.gnews.io/295c2711204e9f904b3727bf50af4c32"
+        }
+      ]
     }
   }
 
@@ -48,21 +58,32 @@ class App extends Component {
   render(){
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="wrap">
+        <header>
+          <div className="header-content">
+            <div className="search">
+              <form action="#">
+                <div className="form-group">
+                  <input type="text" id="search" placeholder="Type your topic here"/>
+                  <div className="btn">
+                    <button type="submit"><i class="fas fa-search"></i></button>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="logo">
+              <img src={logo} alt="" />
+            </div>
+          </div>
         </header>
+        <main>
+          <section className="top-news">
+            <div className="feature-article">
+              <img src={this.state.articles[0].image} alt=""/>
+            </div>
+          </section>
+          <section className="category-news"></section>
+        </main>
       </div>
     )
   }
