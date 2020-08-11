a2052338d8b1efc375e657298c0037c198c2d45a
Mike Jackson-Potter <nzmikejp@gmail.com>
2020-08-11 23:12:17 +1200

    Feature articles

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ class App extends Component {
           title: "COVID-19 testing 'very likely' when Royal Caribbean returns to cruising, executive says",
           description: "Royal Caribbean Group is set to begin COVID-19 testing on its ships when cruise operations return, according to an executive.",
           publishedAt: "2020-08-10 18:14:00 UTC",
-          image: "https://images.gnews.io/295c2711204e9f904b3727bf50af4c32"
+          image: "https://images.gnews.io/295c2711204e9f904b3727bf50af4c32",
+          url: "https://www.thestreet.com"
         }
       ]
     }
@@ -79,7 +80,13 @@ class App extends Component {
         <main>
           <section className="top-news">
             <div className="feature-article">
-              <img src={this.state.articles[0].image} alt=""/>
+              <div className="feature-image">
+                <img src={this.state.articles[0].image} alt=""/>
+                <p className="feature-date"><span>Published: </span>{this.state.articles[0].publishedAt}</p>
+              </div>
+              <h1>{this.state.articles[0].title}</h1>
+              <p>{this.state.articles[0].description}</p>
+              <a href={this.state.articles[0].url} className="btn btn-more" target="_blank">Read more</a>
             </div>
           </section>
           <section className="category-news"></section>
